87d17382b70ac04b06f8016e6445a447411a1627
马金凤 <1021008546@qq.com>
2016-03-28 20:18:45 +0800

    登录APP，adb命令hierarchyviewer工具找ID，手机触摸控制

--- android_SDK_test.py
+++ android_SDK_test.py
@@ -1,25 +1,26 @@
 #coding=utf-8
-#emulator -avd test431
-#E:\windows-x86-SDK\sdk\tools
-#monkeyrunner test.py
-
-
-
 import sys
 from com.android.monkeyrunner import MonkeyRunner as mr
 from com.android.monkeyrunner import MonkeyDevice as md
-from com.android.monkeyrunner import MonkeyImage as  mi
+
+from com.android.monkeyrunner.easy import EasyMonkeyDevice as emd
+from com.android.monkeyrunner.easy import By
+#from com.android.monkeyrunner import MonkeyImage as  mi
+import os
 
 class MyMonkeyrunner:
     def __init__(self):
         self.device = None
+        self.easy_device = None
         self.apk_url = 'E:/windows-x86-SDK/sdk/platform-tools/9358mgr_v2.5.1.12_xiaomodo_201602021454.apk'
         self.apk_name_activity = "com.xmd.manager/com.xmd.manager.window.LoginActivity"
-        self.shot = 5
+        self.tool = 'E:/windows-x86-SDK/sdk/tools'
+        self.shot = 0
 
 
     def connect_device(self):
         self.device = mr.waitForConnection(2.0)
+        self.easy_device = emd(self.device)
         if not self.device:
             print "connect error"
         else:
@@ -31,16 +32,24 @@ class MyMonkeyrunner:
     def start_activity(self):
         self.device.startActivity( component=self.apk_name_activity )
         print "start_activity"
-        mr.sleep(10.0)
+        mr.sleep(1.0)
 
     def do_something(self):
         #self.device.type('helloworld')
-        self.device.press('KEYCODE_HOME', md.DOWN_AND_UP)
+        self.easy_device.touch(By.id('id/username'),md.DOWN_AND_UP)
+        text = self.easy_device.getText(By.id('id/username'))
+        self.device.press('KEYCODE_MOVE_END', md.DOWN_AND_UP)
+        for i in range(len(text)):
+            self.device.press('KEYCODE_DEL', md.DOWN)
+        self.device.type('93')
+        self.easy_device.touch(By.id('id/password'),md.DOWN_AND_UP)
+        self.device.type('123456')
+        self.easy_device.touch(By.id('id/login'),md.DOWN_AND_UP)
         print "do_something"
-        mr.sleep(3.0)
 
     def take_screem(self):
-        mr.sleep(3.0)
+        os.chdir(self.tool)
+        mr.sleep(1.0)
         result = self.device.takeSnapshot()
         result.writeToFile('./shot%d.png'% self.shot,'png')
         self.shot += 1
